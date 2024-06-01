1d9dadd1b2a5beb3e5a009f2b7336d3bdc788516
toriqulmahal6359 <toriqulmahal6359@yahoo.com>
2024-06-01 21:24:34 +0600

    Updated Version v2

--- restAPIProject/settings.py
+++ restAPIProject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 import environ
 import os
 
@@ -110,7 +111,10 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+database_url = env('DATABASE_URL')
+DATABASES["default"] = dj_database_url.parse(database_url)
 
+# 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
